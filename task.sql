b043a29864e10fb5ef23f0986e2b8b331656223e
aisen <djetsubo1337@gmail.com>
2026-03-26 19:10:50 +0200

    Solution 4.0.0

--- task.sql
+++ task.sql
@@ -12,8 +12,8 @@ CREATE TABLE Countries (
 CREATE TABLE GeoIPCache (
     ID INT,
     IP_Range VARCHAR(50),
-    CountryID INT,
-) ENGINE=MEMORY;
+    CountryID INT
+) ENGINE=Memory;
 
 -- Create a table for storing product descriptions for different countries (Columns: ID, CountryID, ProductID, Description )
 CREATE TABLE ProductDescription (
@@ -34,9 +34,7 @@ CREATE TABLE Logs (
 
 -- Create a table for storing reporting data, which will be send to a separate application in the CSV format for analytics purposes (Columns:  Date, ProductName, Orders)
 CREATE TABLE ProductReporting (
-    ID INT,
-    Date DATE NOT NULL ,
-    ProductName VARCHAR(50) NOT NULL ,
-    Orders INT NOT NULL ,
-    PRIMARY KEY (ID)
+    Date DATE NOT NULL,
+    ProductName VARCHAR(50) NOT NULL,
+    Orders INT NOT NULL
 ) ENGINE=CSV;

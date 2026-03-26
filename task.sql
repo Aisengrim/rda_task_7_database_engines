c560906edb86f214f3beeb9a80312fbabf75b6d2
aisen <djetsubo1337@gmail.com>
2026-03-26 18:57:41 +0200

    Solution 2.0.0

--- task.sql
+++ task.sql
@@ -9,13 +9,13 @@ CREATE TABLE Countries (
 ) ENGINE=InnoDB;
 
 -- Create a table for caching GeoIP data (Columns: ID, IP Range, CountryID)
-CREATE TABLE  GeoIP (
+CREATE TABLE  GeoIPCache (
     ID INT,
     IP_Range VARCHAR(50),
     CountryID INT,
-    FOREIGN KEY (CountryID) REFERENCES Countries (ID),
+    CONSTRAINT FK_IPRangeCountry FOREIGN KEY (CountryID) REFERENCES Countries(ID),
     PRIMARY KEY (ID)
-) ENGINE=Memory;
+) ENGINE=MEMORY;
 
 -- Create a table for storing product descriptions for different countries (Columns: ID, CountryID, ProductID, Description )
 CREATE TABLE ProductDescription (
@@ -23,22 +23,23 @@ CREATE TABLE ProductDescription (
     CountryID INT,
     ProductID INT,
     Description VARCHAR(100),
-    FOREIGN KEY (CountryID) REFERENCES Countries (ID),
+    CONSTRAINT FK_ProductDescriptionCountry FOREIGN KEY (CountryID) REFERENCES Countries(ID),
     PRIMARY KEY (ID)
 ) ENGINE=MyISAM;
 
 -- Create a table for storing logs. For now we don't need to save them, but we need to implement functionality (Columns: ID, Time, LogRecord)
 CREATE TABLE Logs (
     ID INT,
-    Time TIME,
-    LogRecord VARCHAR(100),
+    Timestamp TIMESTAMP,
+    Message VARCHAR(255),
     PRIMARY KEY (ID)
 ) ENGINE=BLACKHOLE;
+
 -- Create a table for storing reporting data, which will be send to a separate application in the CSV format for analytics purposes (Columns:  Date, ProductName, Orders)
 CREATE TABLE ProductReporting (
     ID INT,
-    Date DATE,
-    ProductName VARCHAR(50),
-    Orders INT,
+    Date DATE NOT NULL ,
+    ProductName VARCHAR(50) NOT NULL ,
+    Orders INT NOT NULL ,
     PRIMARY KEY (ID)
 ) ENGINE=CSV;
